aaaab0bd6751937a9dc7d85f01591cafd31d4441
Brandon Nelson <nelsonb3@oregonstate.edu>
2023-11-26 18:53:54 -0800

    separated out play games class. Each game has its own class to initiate a play call to, which game is played is now directly called rather than by switch game key

--- exercise-8.2.rb
+++ exercise-8.2.rb
@@ -1,15 +1,17 @@
-require_relative 'playgames'
+require_relative 'playpoker'
+require_relative 'playchess'
+require_relative 'playgo'
 
-poker_game = PlayGames.new(1, ["alice", "bob", "chris", "dave"])
+poker_game = PlayPoker.new(["alice", "bob", "chris", "dave"])
 poker_game.play()
 
 puts
 
-chess_game = PlayGames.new(2, [["alice", "white"], ["bob", "black"]])
+chess_game = PlayChess.new([["alice", "white"], ["bob", "black"]])
 chess_game.play()
 
 puts
 
-go = PlayGames.new(3, [["alice", "white"], ["bob", "black"]])
+go = PlayGo.new([["ron", "white"], ["jack", "black"]])
 go.play()
 
